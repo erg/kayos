483658d735f9102e78f43e4959b4d0a82063a640
Doug Coleman <doug.coleman@gmail.com>
2014-11-21 23:37:38 -0800

    kayos: Forward buffer to writer. Use strtok_r to parse buffer.

--- src/kayos_client.c
+++ src/kayos_client.c
@@ -19,24 +19,29 @@ void client_loop() {
 	int stop = 0;
 	do {
 		char buffer[4096];
+		ssize_t ret;
+		ssize_t written = 0;
+		ssize_t nbytes;
+
 		fprintf(stderr, "client: loop head\n");
-		ssize_t nbytes = safe_read(0, buffer, sizeof(buffer));
+		nbytes = safe_read(0, buffer, sizeof(buffer));
 		if(nbytes == -1) {
 			fatal_error("client safe_read");
 		} else if(nbytes == 0) {
 			fprintf(stderr, "client: client dc!\n");
 			break;
 		}
+
 		fprintf(stderr, "client: got %zd bytes, buffer: %s\n", nbytes, buffer);
 		named_hexdump(stderr, "client got", buffer, nbytes);
 
-		fprintf(stdout, "hello writer from client\r\n");
+		//fprintf(stdout, "hello writer from client\r\n");
+		safe_write(1, buffer, nbytes, &written);
 		fflush(stdout);
 
 		stop = handle_buffer(buffer, sizeof(buffer));
 
-		ssize_t written = 0;
-		ssize_t ret = safe_write(0, "ok\n", 3, &written);
+		ret = safe_write(0, "ok\n", 3, &written);
 		(void) ret;
 	} while(!stop);
 }
--- src/kayos_writer.c
+++ src/kayos_writer.c
@@ -9,19 +9,35 @@
 #include "io.h"
 #include "utils.h"
 
+//char * strtok_r(char *restrict str, const char *restrict sep, char **restrict lasts);
+
+//<command name> <key> <flags> <exptime> <bytes> [noreply]\r\n
+//cas <key> <flags> <exptime> <bytes> <cas unique> [noreply]\r\n
+
+// cas, set, add, replace, append, prepend
+
+
+
 int handle_buffer(char *buffer, ssize_t len) {
 	int stop = 0;
 
-	if(!strncmp(buffer, "exit", len))
-		stop = 1;
+	char *sep = " \t\r\n";
+	char *word, *phrase, *brkt, *brkb;
 
+	word = strtok_r(buffer, sep, &brkt);
+	while(word) {
+		fprintf(stderr, "word: %s\n", word);
+		word = strtok_r(0, sep, &brkt);
+	}
+	//if(!strncmp(buffer, "exit", len))
+		//stop = 1;
 	return stop;
 }
 
 void writer_loop(fdb_file_handle *dbfile, fdb_kvs_handle *db) {
 	int stop = 0;
+	char buffer[1024];
 	do {
-		char buffer[4096];
 		fprintf(stderr, "writer: loop head\n");
 		ssize_t nbytes = safe_read(0, buffer, sizeof(buffer));
 		if(nbytes == 0)
@@ -29,12 +45,12 @@ void writer_loop(fdb_file_handle *dbfile, fdb_kvs_handle *db) {
 		else if(nbytes == -1) {
 			fatal_error("writer: safe_read");
 		}
-        named_hexdump(stderr, "writer got", buffer, nbytes);
+		named_hexdump(stderr, "writer got", buffer, nbytes);
 		stop = handle_buffer(buffer, sizeof(buffer));
 
-        ssize_t written = 0;
-        ssize_t ret = safe_write(0, "ok\n", 3, &written);
-        (void) ret;
+		ssize_t written = 0;
+		ssize_t ret = safe_write(0, "ok\n", 3, &written);
+		(void) ret;
 	} while(!stop);
 }
 
@@ -45,18 +61,12 @@ int main(int argc, char *arg[]) {
 
 	fdb_config fconfig = fdb_get_default_config();
 	fdb_kvs_config kvs_config = fdb_get_default_kvs_config();
-	//if (comp_type == DAEMON_COMPACTION) {
-		//fconfig.compaction_mode = FDB_COMPACTION_AUTO;
-		//fconfig.compaction_threshold = 10;
-		//fconfig.compactor_sleep_duration = 5;
-	//}
 	status = fdb_open(&dbfile, "./test.fdb", &fconfig);
 	if(status != FDB_RESULT_SUCCESS)
 		fatal_error("fdb_open failed");
 	status = fdb_kvs_open_default(dbfile, &db, &kvs_config);
 	if(status != FDB_RESULT_SUCCESS)
 		fatal_error("fdb_kvs_open_default failed");
-
 
 	writer_loop(dbfile, db);
 
